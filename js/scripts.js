0356758e6a8146a6dcb935827e688a9f40aa236e
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-01-15 14:17:45 -0800

    put back in code previously deleted for debugging purposes.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,31 @@ $(document).ready(function() {
   console.log("Script executing!");
   $("#formOne").submit(function(event) {
     console.log("Submit form successfully reached.");
-    
+    const ruby = $("input#gemology").val();
+    const cSharp = $("input#music").val();
+    const python = $("input#herpetology").val();
+    const javaScript = $("input#coffee").val();
+    const swift = $("input#run").val();
+
+    if (ruby === "yes") {
+      $(".card").hide();
+      $("#aboutRuby").show();
+    } else if (cSharp === "yes") {
+      $(".card").hide();
+      $("#aboutC").show();
+    } else if (python === "yes") {
+      $(".card").hide();
+      $("#aboutPython").show();
+    } else if (javaScript === "yes") {
+      $(".card").hide();
+      $("#aboutJavaScript").show();
+    } else if (swift === "yes") {
+      $(".card").hide();
+      $("#aboutSwift").show();
+    } else {
+      $(".card").hide();
+      $("#aboutGo").show();
+    }
     event.preventDefault();
 
   });
